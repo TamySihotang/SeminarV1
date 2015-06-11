c31b61e6825ae378e5b398880fcc3c01d7cf8567
TamySihotang <sihotangtamy@gmail.com>
2015-06-11 08:01:55 +0700

    Model versi 2

--- backend/views/news/_form.php
+++ backend/views/news/_form.php
@@ -2,6 +2,12 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+ use dosamigos\ckeditor\CKEditor;
+//use himiklab\ckeditor\CKEditor;
+use common\models\News;
+//use letyii\tinymce\Tinymce;
+use moonland\tinymce\TinyMCE;
+use kartik\widgets\FileInput;
 
 /* @var $this yii\web\View */
 
@@ -19,8 +25,27 @@ use yii\widgets\ActiveForm;
  
     <?= $form->field($model, 'title')->textInput(['maxlength' => 200]) ?>
     <?= $form->field($model, 'tanggal_penting')->textInput(['maxlength' => 200]) ?>
-    <?= $form->field($model, 'picture')->fileInput(); ?>
-    <?= $form->field($model, 'content')->textarea(['cols' => 6, 'rows' => 10, 'maxlength' => 10000]) ?>
+    <?= $form->field($model, 'picture')->widget(FileInput::classname(),[
+    'options' => ['multiple' => true],
+    'pluginOptions' =>[
+        'browseClass' => 'btn btn-success',
+        'uploadClass' => 'btn btn-info',
+        'removeClass' => 'btn btn-danger',
+        'removeIcon' => '<i class="glyphicon glyphicon-trash"></i>',
+        'allowedFileExtensions' => ['png','jpg'],
+    ],
+
+    ]);
+
+ ?>
+        <?php
+    	// $model = News::findOne(1);
+	echo $form->field($model, 'content')->widget(CKEditor::className(), [
+        'preset' => 'full'
+    ]) 
+    ?>
+
+   
 
 <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-default']) ?>
